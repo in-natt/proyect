ee271d7399308383cdd72c6bdab346b78ac88dad
Natt Forclaz <natt.for.m@hotmail.com>
2020-10-06 14:41:11 -0300

    Dandole forma a mi programa.py

--- programa.py
+++ programa.py
@@ -1,1 +1,19 @@
-print "hola mundo"
+print ("Mis programas en Python")
+
+#texto = "Esto es el resultado: "
+acum = 0
+
+#Define variables
+#num1 = int(input("Ingrese un numero: "))
+num2 = 20
+num3 = 30
+
+#acum = acum + num1 + num2 + num3
+
+#print (texto + str(acum))
+
+for x in range(0,6,2):
+  print(x)
+
+#numero = int(input(“Introduce un numero: “))
+#print (“El numero que ingreso es:\n”,numero)
